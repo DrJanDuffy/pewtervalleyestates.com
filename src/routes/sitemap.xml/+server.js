8c6d932cf0f19f6c158dba07b15a897d6304d3f4
genekellyboyle <DrDuffy@bhhsnv.com>
2025-12-08 21:55:40 -0800

    Update sitemap.xml endpoint and clean up todos route

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,42 +1,48 @@
-import { generateSitemapData } from "$lib/seo.js"
+// src/routes/sitemap.xml/+server.js
+
+const site = 'https://www.pewtervalleyestates.com';
+
+const pages = [
+  '',
+  '/homes-for-sale',
+  '/buy-a-home',
+  '/buyer-checklist',
+  '/listings',
+  '/investment-properties',
+  '/sell-your-home',
+  '/home-valuation',
+  '/market-analysis',
+  '/selling-checklist',
+  '/neighborhood',
+  '/southwest-las-vegas',
+  '/neighborhood-guide',
+  '/photos',
+  '/las-vegas-relocation-guide',
+  '/about',
+  '/contact',
+  '/richmond-demo',
+  '/dr-jan-duffy',
+  '/google-reviews',
+  '/faq'
+];
 
 export async function GET() {
-  const sitemapData = generateSitemapData()
+  const today = new Date().toISOString().split('T')[0];
+  
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages.map(page => `  <url>
+    <loc>${site}${page}</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>${page === '' ? 'daily' : 'weekly'}</changefreq>
+    <priority>${page === '' ? '1.0' : page.includes('homes-for-sale') || page.includes('listings') ? '0.9' : '0.8'}</priority>
+  </url>`).join('\n')}
+</urlset>`;
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml"
-        xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
-        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
-        xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-${sitemapData
-  .map(
-    (page) => `  <url>
-    <loc>${page.url}</loc>
-    <lastmod>${page.lastmod}</lastmod>
-    <changefreq>${page.changefreq}</changefreq>
-    <priority>${page.priority}</priority>
-    <mobile:mobile/>
-    ${
-      page.url.includes("/listings")
-        ? `
-    <image:image>
-      <image:loc>${page.url}/og-image.jpg</image:loc>
-      <image:title>Pewter Valley Estates Las Vegas - Property Listings</image:title>
-      <image:caption>Modern new construction homes in Las Vegas, Nevada</image:caption>
-    </image:image>`
-        : ""
-    }
-  </url>`
-  )
-  .join("\n")}
-</urlset>`
-
-  return new Response(sitemap, {
+  return new Response(xml, {
     headers: {
-      "Content-Type": "application/xml",
-      "Cache-Control": "public, max-age=3600",
-    },
-  })
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'max-age=3600'
+    }
+  });
 }
